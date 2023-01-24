c8b676be9ec6e83f38e00973a55dcc6aab406249
Csigusz22 <98027635+Csigusz22@users.noreply.github.com>
2023-01-24 10:32:43 +0100

    Add files via upload

---
+++ igen.py
@@ -0,0 +1,56 @@
+import random
+
+def szoBeker():
+    szo=input("Kérek egy szót: ")
+    if szo=="":
+        jelentes=""
+    else:
+        jelentes=input(szo+ " jelentése: ")
+
+    return [szo,jelentes]
+
+szavak=[]
+def sokBeker():
+    szo=szoBeker()
+    while szo[0]!="":
+        szavak.append(szo)
+        szo=szoBeker()
+    return szavak
+    
+def filebaIr(lista):
+    f=open("szotar.txt","a")
+
+    for e in lista:
+        #print(e)
+        f.write(" ".join(e))
+        f.write("\n")
+    f.close()
+
+kerdesek=[]
+def beolvas():
+    f=open("szotar.txt","r")
+    for sor in f:
+        kerdesek.append(sor.replace("\n","").split(" "))
+
+    f.close()
+def kerdez():
+    valasztott=random.choice(kerdesek)
+    print("valasztott", valasztott)
+    rossz=[]
+    for i in range(3):
+        temp=random.choice(kerdesek)
+        print(temp)
+        while temp not in rossz and temp!=valasztott:
+            rossz.append(temp)
+        print(rossz)
+
+    print("-"*40)
+    print("Mit jelenet ez a szó? "+ valasztott[0])
+
+    rossz.append(valasztott)
+    print(rossz)
+
+beolvas()
+kerdez()
+#szavak=sokBeker()
+#filebaIr(szavak)

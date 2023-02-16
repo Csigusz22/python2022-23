59bccf98445757dd24b83c4e840fc2479c2b14e7
Csigusz22 <98027635+Csigusz22@users.noreply.github.com>
2023-02-16 13:08:40 +0100

    Add files via upload

---
+++ retségi feladat.py
@@ -0,0 +1,11 @@
+print("1 feladat")
+szoveg=input("Kérek egy szöveget.")
+
+szam=""
+#while type(szam)!=int:
+while szam="":
+try:
+    szam=int(input("kérek egy egész számot."))
+except:
+    print("Ez nem egész szám!")
+    

6975ecb0effa57896ebd77d4b31715d0845f61a5
Csigusz22 <98027635+Csigusz22@users.noreply.github.com>
2023-02-17 09:39:09 +0100

    Add files via upload

---
+++ 2 feladat.py
@@ -0,0 +1,9 @@
+import haromszog
+
+def ujabb(darab):
+    haromszogek[]
+    for i in range(darab):
+        haromszogek.append(haromszog.haromszog())
+        
+#lista=haromszog.haromszog()
+#print(lista)

9c4f70c31f2182b10775243d8a9e4082cd9994ea
Csigusz22 <98027635+Csigusz22@users.noreply.github.com>
2023-02-22 13:20:08 +0100

    Add files via upload

---
+++ épitményadó.py
@@ -0,0 +1,40 @@
+def ado(adosav,alapterulet):
+    ar=0
+    if adosav=="A":
+        ar int(arak[0])*alapterulet
+    elif adosav=="B"
+        ar int(arak[1])*alapterulet
+    else:
+        ar int(arak[2])*alapterulet
+    if ar>=1000:
+    else:
+        
+
+        
+f=open("utca.txt")
+hazak=[]
+
+for e in f:
+    hazak.append(e.replace("\n","").split(" "))
+
+f.close
+
+arak=hazak.pop(0)
+print("2. feladat. A mintában {} telek szerepel.".format(len(hazak)))
+
+adosza=int(input("3. feladat. Egy tulajdonos adószám:")
+tulajdonok=[]
+for e in hazak:
+    if e[0]==adoszam:
+        tulajdonok.append[e]
+
+if len(tulajdonok)==0:
+    print("Nem szerepel az adatállományban.")
+else:
+    for e in tulajdonok:
+    print("{} utca {}".format(e[1],e[2]))
+
+hazakA=[efor e in hazak if e[3]=="A"]
+    
+
+    
